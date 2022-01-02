a799e2466b65e199bd9aa7c973c6b1c0ce39b9cd
sami97-alnaji <samisad123@gmail.com>
2022-01-02 18:59:54 +0200

    db 39 edit support class for serch

--- lib/Packages/Pages/Research/Components/support/support_class.dart
+++ lib/Packages/Pages/Research/Components/support/support_class.dart
@@ -31,7 +31,7 @@ class Person {
     var x = await Register().getSerchePage();
 
     for (int i = 0; i < x.length; i++) {
-<<<<<<< HEAD
+
       int q=int.parse(x[i]['account_type']);
       String? stt='';
       switch(q){
@@ -46,20 +46,7 @@ class Person {
         case(50):stt='سواق';
         break;
       }
-      if(q==10){
 
-
-      }else{
-
-=======
-      int q = int.parse(x[i]['account_type']);
-      String? stt = '';
-      if (q == 10) {
-        stt = 'مهندس';
-      } else {
-        stt = 'سواق';
->>>>>>> 1e656901e768f5dd1d82f2d7638873c16889ddc3
-      }
       people.add(Person(
         age: int.tryParse(x[i]['user_no']),
         name: x[i]['first_name'] + ' ' + x[i]['last_name'],
